071333d5a59fe46ccae9b53d089bb56ddde58d26
Paul Serby <paul.serby@clock.co.uk>
2012-03-20 20:31:37 +0000

    Completing split functionality

--- public/js/yonder-client.js
+++ public/js/yonder-client.js
@@ -1,35 +1,91 @@
 (function($) {
   window.yonder = function(options) {
 
-    var $el = $('body')
+    var log = function() { console.log(arguments); }
+      , $el = $('body')
       , yindow
       , pane = 0
-      , socket = io.connect(document.location.origin);
+      , socket = io.connect(document.location.origin)
+      , rootContainer;
 
     socket.emit('register', { name: options.name });
 
     function setup(data) {
+      log('Setup', data);
+      $el.html('');
+      update(data);
+      setPanesContent(data.container);
+    }
+
+    function update(data) {
+      log('Update', data);
       yindow = data;
-      yindow.panes.forEach(function(pane, index) {
-        $el.html('<div id="pane-' + index + '" class="pane"><div style="display: none"></div><iframe style="display: none"></iframe></div>');
+      setupPanes(yindow.container);
+    }
+
+    function addPane(pane) {
+      var $pane = $('#pane-' + pane.index);
+      if ($pane.length === 0) {
+        $el.append('<div id="pane-' + pane.index + '" class="pane"><div style="display: none"></div><iframe style="display: none"></iframe></div>');
         setPane(pane);
+      }
+    }
+
+    function setupPanes(container) {
+      container.items.forEach(function(item) {
+        if (item.orientation) {
+          setupPanes(item);
+        } else {
+          addPane(item);
+        }
       });
       redraw();
     }
 
+    function setPanesContent(container) {
+      container.items.forEach(function(item) {
+        if (item.orientation) {
+          setPanesContent(item);
+        } else {
+          setPane(item);
+        }
+      });
+    }
+
+    function redrawContainer(container, top, left, width, height) {
+      var paneWidth = width
+        , paneHeight = height
+        , paneTop = top
+        , paneLeft = left;
+
+      console.log('Redraw container', arguments);
+
+      if (container.orientation === 'h') {
+        paneHeight = Math.round(height / container.items.length);
+      } else if (container.orientation === 'v') {
+        paneWidth = Math.round(width / container.items.length);
+      }
+
+      container.items.forEach(function(item) {
+        if (item.orientation) {
+          redrawContainer(item, paneTop, paneLeft, paneWidth, paneHeight);
+        } else {
+          var $pane = $('#pane-' + item.index);
+          $pane.css({ top: paneTop, left: paneLeft, width: paneWidth, height: paneHeight, });
+        }
+        paneTop += (container.orientation === 'h' ? paneHeight : 0);
+        paneLeft += (container.orientation === 'v' ? paneWidth : 0);
+      });
+    }
+
     function redraw() {
       console.log('Redraw');
-      yindow.panes.forEach(function(pane, index) {
-        $('#pane-' + index)
-          .width($(window).width())
-          .height($(window).height());
-      });
+      redrawContainer(yindow.container, 0, 0, $(window).width(), $(window).height());
     }
 
     function setPane(data) {
       console.log('Setting panel ' + data.index  + ' to ' + data.url);
       var setThisPane = function() {
-        console.log('data', data, $('#pane-' + data.index));
         var $iframe =  $('#pane-' + data.index + ' iframe')
           , $div = $('#pane-' + data.index + ' div');
         if (data.url) {
@@ -50,6 +106,7 @@
 
     socket.on('setup', setup);
     socket.on('setPane', setPane);
+    socket.on('update', update);
 
     $(window).on('resize', redraw);
 
