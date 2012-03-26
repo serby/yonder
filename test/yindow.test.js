8cfea6b12cff3d0f4f96a9a5bef50e8e1a858dcb
Paul Serby <paul.serby@clock.co.uk>
2012-03-26 22:10:32 +0100

    Fixing tests

--- test/yindow.test.js
+++ test/yindow.test.js
@@ -1,14 +1,16 @@
 describe('yindow', function() {
 
-  it('should be created with a single horizontal container with a single pane', function() {
+  it('should be created with a yindow called y1 with a single horizontal container with a single pane', function() {
     var yindow = require('../lib/yindow').createYindow({name:'y1'});
-    yindow.should.eql({name:'y1',container:{orientation:"h",items:[{url:"/yonder",index:0}]}});
+    yindow.container.orientation.should.eql('h');
+    yindow.name.should.eql('y1');
+    yindow.container.items.length.should.eql(1);
   });
 
   describe('#find()', function() {
     it('should find the 0 index pane in a new yindow', function() {
       var yindow = require('../lib/yindow').createYindow({name:'y1'});
-      yindow.find(0).should.eql({ url: '/yonder', index: 0 });
+      yindow.find(0).url.should.eql('/yonder');
     });
   });
 
@@ -16,21 +18,25 @@ describe('yindow', function() {
     it('should create a second pane in the horizontal container', function() {
       var yindow = require('../lib/yindow').createYindow({name:'y1'});
       yindow.hsplit(0, { url: 'http://test' });
-      yindow.should.eql({name:'y1',container:{orientation:"h",items:[{url:"/yonder","index":0},{url:"http://test",index:1}]}});
+      yindow.container.orientation.should.eql('h');
+      yindow.container.items.length.should.eql(2);
     });
 
     it('should create a third pane under 0 in a horizontal container', function() {
       var yindow = require('../lib/yindow').createYindow({name:'y1'});
       yindow.hsplit(0, { url: 'http://test' });
       yindow.hsplit(0, { url: 'http://third' });
-      yindow.should.eql({name:"y1",container:{orientation:"h",items:[{orientation:"h",items:[{url:"/yonder",index:0},{url:"http://third",index:2}]},{url:"http://test",index:1}]}});
+      yindow.container.orientation.should.eql('h');
+      yindow.container.items.length.should.eql(2);
+      yindow.container.items[0].items.length.should.eql(2);
     });
 
     it('should correctly split a vertical container', function() {
       var yindow = require('../lib/yindow').createYindow({name:'y1'});
       yindow.vsplit(0, { url: 'http://1' });
       yindow.hsplit(1, { url: 'http://2' });
-      yindow.should.eql({name:"y1",container:{orientation:"v",items:[{url:"/yonder",index:0},{orientation:"h",items:[{url:"http://1",index:1},{url:"http://2",index:2}]}]}});
+      yindow.container.orientation.should.eql('v');
+      yindow.container.items[1].items[1].url.should.eql('http://2');
     });
 
     it('should correctly split a vertical container already in a horizontal container', function() {
@@ -39,7 +45,8 @@ describe('yindow', function() {
       yindow.vsplit(0, { url: 'http://2' });
 
       yindow.hsplit(0, { url: 'http://0' });
-      yindow.should.eql({"container":{"orientation":"h","items":[{"orientation":"v","items":[{"orientation":"h","items":[{"url":"/yonder","index":0},{"url":"http://0","index":3}]},{"url":"http://2","index":2}]},{"url":"http://1","index":1}]},"name":"y1"});
+      yindow.container.orientation.should.eql('h');
+      yindow.container.items[0].orientation.should.eql('v');
     });
   });
 
@@ -48,7 +55,8 @@ describe('yindow', function() {
       var yindow = require('../lib/yindow').createYindow({name:'y1'});
       yindow.hsplit(0, { url: 'http://1' });
       yindow.vsplit(0, { url: 'http://2' });
-      yindow.should.eql({name:"y1",container:{orientation:"h",items:[{orientation:"v",items:[{url:"/yonder",index:0},{url:"http://2",index:2}]},{url:"http://1",index:1}]}});
+      yindow.container.orientation.should.eql('h');
+      yindow.container.items[0].orientation.should.eql('v');
     });
   });
 });
