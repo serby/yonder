f0fa949ea3e74a37a04ece4d158d5a57e35b3612
Dom Harrington <dom@harrington-mail.com>
2013-12-04 15:43:40 +0000

    Add the ability to pass an array of yonders into the constructor

--- test/yonder.test.js
+++ test/yonder.test.js
@@ -24,4 +24,33 @@ describe('yonder', function () {
     stripEventEmitterProps(yonder.find('y1')).should.eql(yonderConfig)
   })
 
+  it('should allow for automatic creation of a yonder when passing an array', function () {
+    var yonderConfig =
+      [ { container:
+          { orientation: 'h'
+          , items:
+            [ { url: '/yonder'
+              , index: 0
+              }
+            ]
+          }
+        , name: 'y3'
+        }
+      , { container:
+          { orientation: 'h'
+          , items:
+            [ { url: '/new-url'
+              , index: 1
+              }
+            ]
+          }
+        , name: 'y4'
+        }
+      ]
+      , yonder = require('../lib/yonder').createYonder(yonderConfig)
+
+    stripEventEmitterProps(yonder.find('y3')).should.eql(yonderConfig[0])
+    stripEventEmitterProps(yonder.find('y4')).should.eql(yonderConfig[1])
+  })
+
 })
